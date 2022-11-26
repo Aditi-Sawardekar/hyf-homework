e6e14e82ab31a4b40b40fad71fb30705bc203285
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2022-11-26 16:10:57 +0100

    Made changes as per suggestions.

--- javascript/javascript2/week1/problem-solving-cardio.js
+++ javascript/javascript2/week1/problem-solving-cardio.js
@@ -1,30 +1,21 @@
 //Problem Solving Cardio
 
 // 1.1. Find the shortest word
-const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
-const shortestWord = shortestDanishWord(danishWords)
 
+const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
+const findShortestWord = shortestDanishWord(danishWords);
 
 function shortestDanishWord(danishWords) {
-    const wordLength = [];
-
-    // Loop through the array to get the length of each word
-    for (let i = 0; i < danishWords.length; i++) {
-        wordLength.push((danishWords[i].length))
-    }
-    //console.log (wordLength)
-
-    const shortestWordLength = (Math.min(...wordLength));
-    //... why is it used
-    console.log(shortestWordLength); //1
-
-    for (let i = 0; i < danishWords.length; i++) {
-        if ((danishWords[i].length) === shortestWordLength) {
-            return (danishWords[i])
-        }
-    }
-};
-console.log(shortestWord);
+    let shortestWord = danishWords[0]
+    const shortestDanishWord = danishWords.filter(element => {   
+        if(element.length<shortestWord.length){
+            return element
+        }  
+    })
+    return shortestDanishWord
+};    
+     
+    console.log (findShortestWord)
 
 // 1.2. Find and count the Danish letters
 //Find the individual number and the total number of Danish letters in a string.
@@ -34,6 +25,20 @@ const numberOfLetters = countDanishLetters(danishString)
 
 const danishString2 = "Blå grød med røde bær";
 const numberOfLetters2 = countDanishLetters(danishString2)
+
+/*
+function countDanishLetters(string) {
+    const counts = {Å: null, Æ: null, Ø: null};
+    let total = 0;
+
+    for (const key in counts) {
+        counts[key] = (string.match(new RegExp(key, "gi")) || []).length;
+        total += counts[key];
+    }
+    counts.total = total;
+    return counts;
+}
+*/
 
 function countDanishLetters(string) {
     let countÅ
