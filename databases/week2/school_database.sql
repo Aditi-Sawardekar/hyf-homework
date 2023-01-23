573a61f032e1c8fbb543547b462b3d3e530bbeb2
Aditi-Sawardekar <aditi.sawardekar@gmail.com>
2023-01-23 12:31:25 +0100

    Made changes in data type and character length as suggested.

--- databases/week2/school_database.sql
+++ databases/week2/school_database.sql
@@ -9,18 +9,18 @@ USE `lesson2_homework_school_database`;
 -- Class: with the columns: id, name, begins (date), ends (date)
 CREATE TABLE IF NOT EXISTS `class` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `begins` DATETIME NOT NULL,
-  `ends` DATETIME NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `begins` DATE NOT NULL,
+  `ends` DATE NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Student: with the columns: id, name, email, phone, class_id (foreign key)
 CREATE TABLE `student` (
   `id` int(3) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` varchar(255) NULL,
+  `name` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `phone` varchar(15) NULL,
   `class_id` int(10) unsigned NOT NULL,  
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_class` FOREIGN KEY (`class_id`) REFERENCES `class` (`id`) ON DELETE CASCADE
